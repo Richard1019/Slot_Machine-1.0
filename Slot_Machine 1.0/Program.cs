2480b28a676d621de9d848a50ad35b943f7fb7ae
Richard1019 <richardviktorik@gmail.com>
2024-10-05 19:20:22 +0200

    Add project files.

---
+++ Slot_Machine 1.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Slot_Machine_1._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
