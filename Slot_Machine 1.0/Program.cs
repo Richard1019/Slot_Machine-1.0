d50b29156cbb829ea3ba12054687d539d8b846ad
Richard1019 <richardviktorik@gmail.com>
2024-10-05 19:26:59 +0200

    Laid down the rules of the game, get the player to bet.

--- Slot_Machine 1.0/Program.cs
+++ Slot_Machine 1.0/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+            int[] symbols = { 1, 2, 3, 4, 5, 6, };
+            int Playerbet = 0;
+
+            Console.WriteLine("\nWanna play some Slot machine..? Of course you do..");
+            Console.WriteLine("\nSo, every horizontal and vertical line doubles your bet, and every line across triples it");
+            Console.WriteLine("Alright, go on, place your bet now! From one dollar to ten!");
+
         }
     }
 }
